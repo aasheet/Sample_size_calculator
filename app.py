5a3ffe20fc917e93bf2d2471cee4427b49c2c3df
aasheet <aasheet.kumar.py@gmail.com>
2024-01-23 15:08:20 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY],
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
+server = app.server
 
 app.layout = dbc.Container([
     dbc.Row([
